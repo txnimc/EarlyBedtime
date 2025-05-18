181305fd7dea2ce74ac637f6ac4e1e29d857cb1a
Toni <toniheddings@gmail.com>
2025-05-17 23:20:14 -0400

    fix mixinextras

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,25 +3,14 @@ plugins {
 }
 
 blahaj {
-	config {
-		// yarn()
-		// versionedAccessWideners()
-	}
+	config { }
 	setup {
 		txnilib("1.0.23")
 		forgeConfig()
-		conditionalMixin()
 
-		/* access Gradle's DependencyHandler
-		deps.modImplementation("maven:modrinth:sodium:mc$mc-0.6.5-$loader")
-
-		// configure Curseforge & Modrinth publish settings
-		incompatibleWith("optifine")
-
-		// add mods with Blahaj's fluent interface
-		addMod("sodiumextras")
-			.modrinth("sodium-extras") // override with Modrinth URL slug
-			.addPlatform("1.21.1-neoforge", "neoforge-1.21.1-1.0.7")
-			.addPlatform("1.21.1-fabric", "fabric-1.21.1-1.0.7") { required() } */
+		if (mod.isForge) {
+			deps.compileOnly(deps.annotationProcessor("io.github.llamalad7:mixinextras-common:0.4.1")!!)
+			deps.implementation(deps.include("io.github.llamalad7:mixinextras-forge:0.4.1")!!)
+		}
 	}
 }
